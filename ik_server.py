208a41ff40b40793e491f894288ffca2fc3d1394
robot-ranger <ted@processrobotics.com>
2025-11-03 12:21:16 -0800

    fix path

--- ik_server.py
+++ ik_server.py
@@ -62,7 +62,7 @@ class PyRoKiPlanner:
 project_root = Path(__file__).parent
 robot_library = project_root / "robots/library"
 
-robot_urdf_path = robot_library / "library/ABB/CRB15000_10kg_152_v1/CRB15000_10kg_152.urdf" 
+robot_urdf_path = robot_library / "ABB/CRB15000_10kg_152_v1/CRB15000_10kg_152.urdf" 
 assert robot_urdf_path.exists(), f"{robot_urdf_path} not exist"
 planner = PyRoKiPlanner(robot_urdf_path, "flange")
 
